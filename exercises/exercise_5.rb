c1572d64fd52588ab10bf404207cdea7b05b4326
Melanie Tong <melaniietong@gmail.com>
2021-03-04 09:28:16 +0000

    Getting revenue for stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@rev_sum = Store.sum(:annual_revenue)
+@rev_sum_avg = @rev_sum / Store.count
+@rev_mil = Store.where("annual_revenue >= 1000000").count
+
+
+puts @rev_sum
+puts @rev_sum_avg
+puts @rev_mil
